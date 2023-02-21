436f3cb302027e9182610afa9217e899b4ba5b0d
Dennis_Mugarte <107282802+DennisMugarte@users.noreply.github.com>
2023-02-21 15:09:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
+///////////////////////////////////////////////////////////////////////////////
+// const express = require('express');
+// const app = express();
+// const morgan = require('morgan');
 
-// SETTINGS
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
+// // SETTINGS
+// app.set('port', process.env.PORT || 3000);
+// app.set('json spaces', 2);
 
-// MIDDLEWARES
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // MIDDLEWARES
+// app.use(morgan('dev'));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// ROUTES
-app.use(require('./routes/index'));
-app.use('/api/movies', require('./routes/movies'));
+// // ROUTES
+// app.use(require('./routes/index'));
+// app.use('/api/movies', require('./routes/movies'));
 
-// STARTING THE SERVER
+// // STARTING THE SERVER
 
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
-});
+// app.listen(app.get('port'), () => {
+//     console.log(`Server on port ${app.get('port')}`);
+// });
+//////////////////////////////////////////////////////////////////
+
+const express = require('express')
+const app = express()
+
+app.listen(3000)
+console.log('Server on port', 3000)
